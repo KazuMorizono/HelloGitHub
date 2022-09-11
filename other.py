da1d9ce211eaaa453fe8ccc10cd1d4e19ebe2a7d
KazuMorizono <morizono.kz@icloud.com>
2022-09-11 13:14:45 +0900

    updated other.py

--- other.py
+++ other.py
@@ -1,1 +1,11 @@
-print("こんにちは, GitHub！！")
+from traceback import print_tb
+from unittest import result
+
+
+# print("こんにちは, GitHub！！")
+
+def test(name):
+  return "Hello, " + name
+
+result = test('Tom')
+print(result)
